ed0c164a66e03d015069e237e05c9a51b64cef8c
Rob Lasell <roblasell@gmail.com>
2014-07-01 12:36:01 -0400

    fixed Procfile

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// filename     : app.js
+// author       : Rob Lasell
+// date created : 1 June 2014
+// date modified: 1 June 2014
+// description  : primary file for Michael Lasell's rkresearch app on heroku
+
+
